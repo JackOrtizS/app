a5738fb9ed1e4d09ec98f37feac5dd03cb79dc48
JackDevOs <osjack01@gmail.com>
2023-10-04 20:37:24 -0500

    ValidateForm add

--- lib/Pages/AddRifa.dart
+++ lib/Pages/AddRifa.dart
@@ -21,6 +21,7 @@ class _AddRifaState extends State<AddRifa> {
   TextEditingController _dateF = TextEditingController();
   DateTime? fechaInicio;
   DateTime? fechaTermino;
+  final _form = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -28,141 +29,190 @@ class _AddRifaState extends State<AddRifa> {
       appBar: AppBar(
         title: Text("Añadir Rifa"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            //Nombre
-            TextField(
-              controller: nombreController,
-              decoration: InputDecoration(
-                  labelText: "Nombre",
-                  hintText: "Ingrese el nombre de la rifa"
+      body: Form(
+        key: _form,
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              //Nombre
+              TextFormField(
+                controller: nombreController,
+                validator: (value) {
+                  if (value == "") {
+                    return "Campo obligatorio";
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: "Nombre",
+                    hintText: "Ingrese el nombre de la rifa"),
               ),
-            ),
 
-            //Campo para la descripcion
-            TextField(
-              controller: descripcionController,
-              decoration: InputDecoration(
-                  labelText: "Descripcion",
-                  hintText: "Ingrese descripcion de la rifa"
+              //Campo para la descripcion
+              TextFormField(
+                controller: descripcionController,
+                validator: (value) {
+                  if (value == "") {
+                    return "Campo obligatorio";
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: "Descripcion",
+                    hintText: "Ingrese descripcion de la rifa"),
               ),
-            ),
 
-            //Campo para el numero de boletos
+              //Campo para el numero de boletos
 
-            TextField(
-              controller: numeroBoletosController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Número de boletos",
-                  hintText: "Ingrese el numero de boletos para la rifa"
+              TextFormField(
+                controller: numeroBoletosController,
+                validator: (value) {
+                  if (value == "") {
+                    return "Campo obligatorio";
+                  }
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Número de boletos",
+                    hintText: "Ingrese el numero de boletos para la rifa"),
               ),
-            ),
 
-            TextField(
-              controller: precioBoletoController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Precio del boleto",
-                  hintText: "Ingrese el precio de boleto para la rifa"
+              TextFormField(
+                controller: precioBoletoController,
+                validator: (value) {
+                  if (value == "") {
+                    return "Campo obligatorio";
+                  }
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Precio del boleto",
+                    hintText: "Ingrese el precio de boleto para la rifa"),
               ),
-            ),
 
-            Row(
-              children: [
-                Expanded(child: TextField(
-                  onChanged: (val){
-                    fechaInicio = DateTime.parse(val);
-                  },
-                  controller: _dateI,
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.calendar_month_rounded),
-                    labelText: "Fecha de inicio de la rifa Rifa",),
-                  onTap: () async {
-                    DateTime? pickeddateI = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100));
+              Row(
+                children: [
+                  Expanded(
+                      child: TextFormField(
+                    onChanged: (val) {
+                      fechaInicio = DateTime.parse(val);
+                    },
+                    controller: _dateI,
+                        validator: (value) {
+                          if (value == "") {
+                            return "Campo obligatorio";
+                          }
+                        },
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.calendar_month_rounded),
+                      labelText: "Fecha de inicio de la rifa Rifa",
+                    ),
+                    onTap: () async {
+                      DateTime? pickeddateI = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100));
 
-                    if(pickeddateI != null){
-                      TimeOfDay? pickedTime = await showTimePicker(
-                        context: context,
-                        initialTime: TimeOfDay.now(),
-                      );
-                      if(pickedTime != null){
-                        DateTime selectedDteTime = DateTime(
-                          pickeddateI.year,
-                          pickeddateI.month,
-                          pickeddateI.day,
-                          pickedTime.hour,
-                            pickedTime.minute
+                      if (pickeddateI != null) {
+                        TimeOfDay? pickedTime = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
                         );
-                      //_dateI.text = DateFormat.yMMMMd(Intl.getCurrentLocale()).format(pickeddateI);
-                        setState(() {
-                          fechaInicio = selectedDteTime;
+                        if (pickedTime != null) {
+                          DateTime selectedDteTime = DateTime(
+                              pickeddateI.year,
+                              pickeddateI.month,
+                              pickeddateI.day,
+                              pickedTime.hour,
+                              pickedTime.minute);
+                          //_dateI.text = DateFormat.yMMMMd(Intl.getCurrentLocale()).format(pickeddateI);
+                          setState(() {
+                            fechaInicio = selectedDteTime;
 
-                          _dateI.text = DateFormat.yMMMMd(Intl.getCurrentLocale())
-                              .add_jm()
-                              .format(selectedDteTime);
-                        });
-
+                            _dateI.text =
+                                DateFormat.yMMMMd(Intl.getCurrentLocale())
+                                    .add_jm()
+                                    .format(selectedDteTime);
+                          });
+                        }
                       }
-                    }
-                  },
-                )
-                )
-              ],
-            ),
-            TextField(
-              onChanged: (val){
-                fechaTermino = DateTime.parse(val);
-              },
-              controller: _dateF,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.calendar_month_rounded),
-                labelText: "Fecha de Termino de la rifa Rifa",),
-              onTap: () async {
-                DateTime? pickeddateT = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100));
+                    },
+                  ))
+                ],
+              ),
+              TextFormField(
+                onChanged: (val) {
+                  fechaTermino = DateTime.parse(val);
+                },
+                controller: _dateF,
+                validator: (value) {
+                  if (value == "") {
+                    return "Campo obligatorio";
+                  }
+                },
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_month_rounded),
+                  labelText: "Fecha de Termino de la rifa Rifa",
+                ),
+                onTap: () async {
+                  DateTime? pickeddateT = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100));
 
-                if(pickeddateT != null){
-                  fechaTermino = pickeddateT;
-                  _dateF.text = DateFormat.yMMMMd(Intl.getCurrentLocale()).format(pickeddateT);
-                }
-              },
-            ),
-          ],
+                  if (pickeddateT != null) {
+                    fechaTermino = pickeddateT;
+                    _dateF.text = DateFormat.yMMMMd(Intl.getCurrentLocale())
+                        .format(pickeddateT);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: ()async{
-
+        onPressed: () async {
           /**QuickAlert.show(
             context: context,
             type: QuickAlertType.success,
             text: 'Felicidades Rifa Añadida',
           );**/
 
-          
-          Map<String, dynamic>rifaData ={
-            "nombre":nombreController.text,
-            "descripcion": descripcionController.text,
-            "numeroBoletos": int.tryParse(numeroBoletosController.text) ?? 0,
-            "precioBoleto": int.tryParse(precioBoletoController.text) ?? 0,
-            "fechaInicio": fechaInicio,
-            "fechaFin":fechaTermino
-          };
-          CollectionReference rifas = FirebaseFirestore.instance.collection('rifas');
-          await rifas.add(rifaData);
-          print("Guardado");
-          Navigator.pop(context);
+          /**if(nombreController.text.isEmpty ||
+          descripcionController.text.isEmpty ||
+          numeroBoletosController.text.isEmpty ||
+          precioBoletoController.text.isEmpty ||
+          fechaInicio == null ||
+          fechaTermino == null
+          ){
+           QuickAlert.show(context: context,
+               type: QuickAlertType.error,
+           text: 'Por favor ingrese informacion en todos los campos');
+          }
+          else{**/
+          var isValid = _form.currentState?.validate();
+          if(isValid == null || isValid == false){
+            return;
+          }else{
+            Map<String, dynamic> rifaData = {
+              "nombre": nombreController.text,
+              "descripcion": descripcionController.text,
+              "numeroBoletos": int.tryParse(numeroBoletosController.text) ?? 0,
+              "precioBoleto": int.tryParse(precioBoletoController.text) ?? 0,
+              "fechaInicio": fechaInicio,
+              "fechaFin": fechaTermino
+            };
+            CollectionReference rifas =
+            FirebaseFirestore.instance.collection('rifas');
+            await rifas.add(rifaData);
+            print("Guardado");
+            Navigator.pop(context);
+          }
+
+          // }
         },
       ),
     );
